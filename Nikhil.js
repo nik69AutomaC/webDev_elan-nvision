046ec4e8484f57091a6a0402e5ae5c2c62b2e05a
nik69AutomaC <96917489+nik69AutomaC@users.noreply.github.com>
2023-02-17 23:08:05 +0530

    Create Nikhil.js

---
+++ Nikhil.js
@@ -0,0 +1,26 @@
+useEffect(() =>
+{ const pc = new RTCPeerConnection() // Rest of your logic here }, [])const peerConnection = new RTCPeerConnection();const dataChannel = peerConnection.createDataChannel('foodOrder');
+
+// Listen for incoming data on the data channel
+dataChannel.addEventListener('message', event => {
+  // Handle the incoming data
+  const data = JSON.parse(event.data);
+  if (data.type === 'order') {
+    // Handle the order
+    console.log('New order received:', data.payload);
+  }
+});
+
+// Send an order to the server
+function sendOrder(order) {
+  // Create a new data object with the order payload
+  const data = {
+    type: 'order',
+    payload: order
+  };
+
+  // Send the data over the data channel
+  dataChannel.send(JSON.stringify(data));
+}
+
+    
